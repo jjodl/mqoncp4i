aa3341fe735c78b111fd4eb603425fccf96617f0
JOSEPH JODL <jjodl@us.ibm.com>
2024-03-16 12:01:32 -0500

    update nativeHA

--- MQonCP4I/setup/nativeha/nativeha-create.sh
+++ MQonCP4I/setup/nativeha/nativeha-create.sh
@@ -8,7 +8,8 @@
 export TARGET_NAMESPACE=$1
 export QMpre="mq"$2
 ####export QMname="mq"$2"ha"
-export QMname=$TARGET_NAMESPACE"-qm-ha"
+export QMInstance=$TARGET_NAMESPACE"-qm-ha"
+export QMname="mq"$2"-qm-ha"
 export CHANNEL="mq"$2"hachl"
 export CHLCAPS="MQ"$2"HACHL"
 export VERSION=9.3.5.0-r1
