3ebc097b2fb8cb7338d689abe05c4b8607d7de1d
JOSEPH JODL <jjodl@us.ibm.com>
2024-03-20 20:31:23 -0500

    update scripts

--- MQonCP4I/streamq/deploy/cleanup.sh
+++ MQonCP4I/streamq/deploy/cleanup.sh
@@ -31,8 +31,8 @@ export QMInstance=$TARGET_NAMESPACE"-mq"$student"strm"
 
 oc delete queuemanager $QMInstance -n $TARGET_NAMESPACE
 oc delete secret streamqqmgrcert -n $TARGET_NAMESPACE
-oc delete configmap $TARGET_NAMESPACE-streammqmqsc -n $TARGET_NAMESPACE
-oc delete route $TARGET_NAMEPSPACE"-mq-traffic-mq-mq"$student"strm-ibm-mq-qm" -n $TARGET_NAMESPACE
+oc delete configmap $TARGET_NAMESPACE-streamqmqsc -n $TARGET_NAMESPACE
+oc delete route $TARGET_NAMESPACE"-mq-traffic-mq-mq"$student"strm-ibm-mq-qm" -n $TARGET_NAMESPACE
 oc delete pvc data-$QMInstance-ibm-mq-0 -n $TARGET_NAMESPACE
 
 #rm streamq.yaml
