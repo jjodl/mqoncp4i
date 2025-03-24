c3d95c2376aa63a60bfcaf4c9b2c26dd64c50fed
JOSEPH JODL <jjodl@us.ibm.com>
2025-03-24 10:08:36 -0500

    update CCR scripts

--- MQonCP4I/MQ_setup.sh
+++ MQonCP4I/MQ_setup.sh
@@ -64,7 +64,7 @@ if [ $NS == "cp4i-mq" ]
   then
    export QMGR_NS=""
    else 
-   export QMGR_NS="-"$TARGET_NAMESPACE
+   export QMGR_NS=$TARGET_NAMESPACE"-"
 fi
 echo "QMGR_NS = " $QMGR_NS
 exit
@@ -111,6 +111,14 @@ chmod +x $HA_DIR"ha-install.sh"
 
 echo "[INFO] nativeHA build yaml script is complete."
 #
+# Build CCR enable script
+#
+( echo 'cat <<EOF' ; cat template/nativeha-enable-crr.sh_template ; echo EOF ) | sh > $HA_DIR"ha-enable-ccr-install.sh"
+
+chmod +x $HA_DIR"ha-enable-ccr-install.sh"
+
+echo "[INFO] nativeHA CCR enable build yaml script is complete."
+#
 # Build the nativeHA CRR build yaml script.
 #
 echo "...."
@@ -127,6 +135,14 @@ export HA_DIR="nativeha/deploy/"
 chmod +x $HA_DIR"recovery-install.sh"
 
 echo "[INFO] nativeHA CRR build yaml script is complete."
+#
+# Build CCR enable script
+#
+( echo 'cat <<EOF' ; cat template/recovery-enable-crr.sh_template ; echo EOF ) | sh > $HA_DIR"recovery-enable-ccr-install.sh"
+
+chmod +x $HA_DIR"recovery-enable-ccr-install.sh"
+
+echo "[INFO] Recovery CCR enable build yaml script is complete."
 #
 # Build the UniCluster build yaml scripts.
 #
